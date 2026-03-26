ebd14b6c0ac813ca443cd28faaed4fe2e646f108
Mariama Sadio <mariamasadio796@gmail.com>
2026-03-26 21:34:20 +0000

    fix routing

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import About from "./About"
 import Projects from "./Projects"
 import Contact from "./Contact"
@@ -17,8 +18,8 @@ function Accueil() {
         </p>
 
         <div className="hero-buttons">
-          <a href="projects" className="btn">Voir mes projets</a>
-          <a href="contact" className="btn-outline">Me contacter</a>
+          <Link to="/projects" className="btn">Voir mes projets</Link>
+          <Link to="/contact" className="btn-outline">Me contacter</Link>
         </div>
       </section>
 
