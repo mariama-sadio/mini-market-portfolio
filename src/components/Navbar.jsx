bf8da00cfabcac428eb05e8596261ef08d1e44a8
Mariama Sadio <mariamasadio796@gmail.com>
2026-03-30 13:02:23 +0000

    navbart responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
-import { NavLink } from "react-router-dom"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { FaBars, FaTimes } from "react-icons/fa"
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <nav className="navbar">
-
       <div className="logo">Portfolio</div>
 
-      <div className="menu">
-        <NavLink to="/">Accueil</NavLink>
-        <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+      <div className={`menu ${menuOpen ? "active" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Accueil</Link>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>À propos</Link>
+        <Link to="/projects" onClick={() => setMenuOpen(false)}>Projets</Link>
+        <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
       </div>
 
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
     </nav>
   )
 }
